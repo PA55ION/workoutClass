fd5c22f46aa4349f79db4e10844741b116c625d0
Mai Campbell <jirallc@gmail.com>
2020-09-25 18:10:10 -0400

    set up route wit react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Browse from "./pages/Browse";
+import Home from "./pages/Home";
+import Current from "./pages/Current";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/browse">
+          <Browse />
+        </Route>
+        <Route path="/current">
+          <Current />
+        </Route>
+      </Switch>
     </div>
   );
 }
